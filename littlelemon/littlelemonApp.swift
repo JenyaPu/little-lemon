02c02080324ae2e2bff5d07e7b81cba7dac3abd7
Evgenii Pustozerov <evgenii.pustozerov@uni-bielefeld.de>
2025-02-17 17:21:25 +0100

    Initial Commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Pustozerov, Evgenii on 17.02.25.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
